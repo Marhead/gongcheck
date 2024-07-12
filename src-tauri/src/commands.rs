5be55405194026997767c9d4bdbc2ac674f1ce78
Marhead <clgns0415@gmail.com>
2024-07-12 13:23:08 +0900

    test for mac

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -12,11 +12,10 @@ pub fn select_directory_for_mac() -> Option<PathBuf> {
 }
 
 #[tauri::command]
-pub fn select_directory() -> Option<PathBuf> {
+pub async fn select_directory(app_handle: tauri::AppHandle) -> Option<String> {
     let home_dir = env::home_dir().unwrap_or_else(|| PathBuf::from("/"));
-    let dialog = tauri::api::dialog::blocking::FileDialogBuilder::new()
+    tauri::api::dialog::blocking::FileDialogBuilder::new()
         .set_directory(home_dir)
-        .pick_folder();
-    
-    dialog
+        .pick_folder()
+        .and_then(|path_buf| path_buf.to_str().map(String::from))
 }
