e822d11f8d78c7381ec7c834dd063c0c7ff4eca5
Marhead <clgns0415@gmail.com>
2024-07-12 18:48:28 +0900

    update command

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,16 +1,6 @@
 use std::env;
 use std::path::PathBuf;
 
-// TODO: Check for Mac
-#[tauri::command]
-pub fn select_directory_for_mac() -> Option<PathBuf> {
-    let file_dialog = tauri::api::dialog::blocking::FileDialogBuilder::new()
-        .set_directory("~")
-        .pick_folder();
-
-    file_dialog
-}
-
 #[tauri::command]
 pub async fn select_directory(app_handle: tauri::AppHandle) -> Option<String> {
     let home_dir = env::home_dir().unwrap_or_else(|| PathBuf::from("/"));
